dc0f3c8eba7e1604b585bb43e5e493add14585a6
Inas Asami El <inasasami11@gmail.com>
2026-02-13 08:52:59 +0700

    pertemuan 1 tugas 2

---
+++ Pertemuan_Minggu_1/REPORT.MD
@@ -0,0 +1,6 @@
+|  | Algorithm and Data Structure |
+|--|--|
+| NIM | 254107020165 |
+| Nama |  Inas Asami El Murtadho |
+| Kelas | TI - 1F |
+| Repository | [link] (https://github.com/inasaeel-dev/PRAKTIKUM-ASD-2026/tree/main) |
--- Pertemuan_Minggu_1/Tugas02.java
+++ Pertemuan_Minggu_1/Tugas02.java
@@ -12,16 +12,54 @@ public class Tugas02 {
             jadwal[i][1] = inas.nextLine();
             System.out.print("Hari : ");
             jadwal [i][2] = inas.nextLine();
+            System.out.print("Jam : ");
+            jadwal [i][3] = inas.nextLine();
         }
     }    
     public static void tampilanSemua(String [][] jadwal) {
         System.out.println("======== DAFTAR JADWAL======== ");
-        System.out.printf("%-20s %-15s %-10s %15",
-        "Nama Matkul", "Ruang", "Hari");
+        System.out.printf("%-20s %-15s %-10s %15s\n",
+        "Nama Matkul", "Ruang", "Hari", "Jam");
         for (int i = 0; i < jadwal.length; i++) {
-            System.out.printf("%-20s %-15s %-10s %-15s\n",
+            System.out.printf("%-20s %-15s %-10s \n%-15s\n",
             jadwal[i][0], jadwal[i][1],
             jadwal[i][2], jadwal[i][3]);
         }
+    }
+    public static void cariHari(String[][] jadwal) {
+        System.out.print("\nMasukkan hari yang dicari: ");
+        String hari = inas.nextLine();
+
+        for (int i = 0; i < jadwal.length; i++) {
+            if (jadwal[i][2].equalsIgnoreCase(hari)) {
+                System.out.println(jadwal[i][0] + " | " +
+                                   jadwal[i][1] + " | " +
+                                   jadwal[i][3]);
+            }
+        }
+    }
+    public static void cariMK(String[][] jadwal) {
+        System.out.print("\nMasukkan nama MK yang dicari: ");
+        String mk = inas.nextLine();
+
+        for (int i = 0; i < jadwal.length; i++) {
+            if (jadwal[i][0].equalsIgnoreCase(mk)) {
+                System.out.println("Ruang: " + jadwal[i][1]);
+                System.out.println("Hari : " + jadwal[i][2]);
+                System.out.println("Jam  : " + jadwal[i][3]);
+            }
+        }
+    }
+    public static void main(String[] args) {
+
+        System.out.print("Masukkan jumlah jadwal: ");
+        int n = Integer.parseInt(inas.nextLine());
+
+        String[][] jadwal = new String[n][4];
+
+        inputJadwal(jadwal);
+        tampilanSemua(jadwal);
+        cariHari(jadwal);
+        cariMK(jadwal);
     }
 }

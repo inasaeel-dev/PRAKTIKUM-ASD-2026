02114c1aa22f9a78d276590c43c8ce742a7a4ed9
Inas Asami El <inasasami11@gmail.com>
2026-02-23 22:48:12 +0700

    percobaan 2 dan pertemuan 2

---
+++ Pertemuan_Minggu_2/MahasiswaMain15.java
@@ -0,0 +1,44 @@
+package Pertemuan_Minggu_2;
+
+public class MahasiswaMain15 {
+        String nama;
+    String nim;
+    String kelas;
+    double ipk;
+
+    void tampilkanInformasi () {
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: " + nim);
+        System.out.println("IPK: " + ipk);
+        System.out.println("Kelas: " + kelas);
+    }
+    void ubahKelas (String kelasBaru) {
+        kelas = kelasBaru;
+    }
+    void updateIpk (double ipkBaru) {
+        ipk = ipkBaru;;
+    }
+    String nilaiKinerja () {
+        if (ipk >= 3.5) {
+            return "Kinerja sangat baik";
+        } else if (ipk >= 3.0) {
+            return "Kinerja baik";
+        } else if (ipk >= 2.0) {
+            return "Kinerja cukup";
+        } else {
+            return "Kinerja kurang";
+        }
+    }
+    public static void main(String[] args) {
+        Mahasiswa15 mhs1 = new Mahasiswa15 ();
+        mhs1.nama = "Muhammad Ali Farhan";
+        mhs1.nim = "2241720171";
+        mhs1.kelas = "SI 2J";
+        mhs1.ipk = 3.55;
+
+        mhs1.tampilkanInformasi();
+        mhs1.ubahKelas("SI 2K");
+        mhs1.updateIpk(3.60);
+        mhs1.tampilkanInformasi();
+    }
+}

e32659196c8306afdae607017631d9c5a84cdea9
Inas Asami El <inasasami11@gmail.com>
2026-04-09 09:14:42 +0700

    Percobaan 1C

--- P6/SortingMain15.java
+++ P6/SortingMain15.java
@@ -4,6 +4,7 @@ public class SortingMain15 {
     public static void main(String[] args) {
         int a [] = {20,10,2,7,12};
         int b [] = {30,20,2,8,14};
+        int c [] = {40,10,4,9,3};
 
         Sorting15 dataurut1 = new Sorting15(a, a.length);
         System.out.println("Data awal 1 ");
@@ -18,5 +19,12 @@ public class SortingMain15 {
         dataurut2.bubbleSort();;
         System.out.println("Data sudah diurutkan dengan BUBBLE SORT (ASC)");
         dataurut2.tampil();
+
+        Sorting15 dataurut3 = new Sorting15(c, c.length);
+        System.out.println("Data awal 3 ");
+        dataurut3.tampil();
+        dataurut3.bubbleSort();;
+        System.out.println("Data sudah diurutkan dengan BUBBLE SORT (ASC)");
+        dataurut3.tampil();
     }
 }

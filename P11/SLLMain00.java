320d9af536dc3c29c046abfe29a3c31693333f18
Inas Asami El <inasasami11@gmail.com>
2026-05-11 23:16:39 +0700

    pertanyaan 3 percobaan 1

--- P11/SLLMain00.java
+++ P11/SLLMain00.java
@@ -28,19 +28,19 @@ public class SLLMain00 {
         //     sll.print();
         // }
 
-        Mahasiswa00 mhs1 = new Mahasiswa00("2001", "Alvaro", "TI-1A", 3.5);
-        Mahasiswa00 mhs2 = new Mahasiswa00("2002", "Bimon", "TI-1B", 3.6);
-        Mahasiswa00 mhs3 = new Mahasiswa00("2003", "Cintia", "TI-1A", 3.7);
-        Mahasiswa00 mhs4 = new Mahasiswa00("2004", "Dirga", "TI-1B", 3.8);
+        // Mahasiswa00 mhs1 = new Mahasiswa00("2001", "Alvaro", "TI-1A", 3.5);
+        // Mahasiswa00 mhs2 = new Mahasiswa00("2002", "Bimon", "TI-1B", 3.6);
+        // Mahasiswa00 mhs3 = new Mahasiswa00("2003", "Cintia", "TI-1A", 3.7);
+        // Mahasiswa00 mhs4 = new Mahasiswa00("2004", "Dirga", "TI-1B", 3.8);
 
-        sll.print();
-        sll.addFirst(mhs4);
-        sll.print();
-        sll.addLast(mhs1);
-        sll.print();
-        sll.insertAfter("Dirga", mhs3);
-        sll.print();
-        sll.insertAt(2, mhs2);
-        sll.print();
+        // sll.print();
+        // sll.addFirst(mhs4);
+        // sll.print();
+        // sll.addLast(mhs1);
+        // sll.print();
+        // sll.insertAfter("Dirga", mhs3);
+        // sll.print();
+        // sll.insertAt(2, mhs2);
+        // sll.print();
     }
 }
